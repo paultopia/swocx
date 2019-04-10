4826c51abd61bae867064dd354f7a4cd4597116f
Paul Gowder <paul.gowder@gmail.com>
2019-04-09 21:36:04 -0500

    can merge docs

--- Sources/swocx/main.swift
+++ Sources/swocx/main.swift
@@ -1,5 +1,48 @@
 import AppKit
 
 
-let data = NSData(contentsOfFile: "testdoc.docx")
-let str = try! NSAttributedString(data: data! as Data, options: [.documentType: NSAttributedString.DocumentType.officeOpenXML, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+
+
+
+func readDocx(_ infile: String) -> NSAttributedString {
+    let data = NSData(contentsOfFile: infile)
+    let str = try! NSAttributedString(data: data! as Data, options: [.documentType: NSAttributedString.DocumentType.officeOpenXML, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+    return str
+}
+
+func readMutableDocx(_ infile: String) -> NSMutableAttributedString {
+    let data = NSData(contentsOfFile: infile)
+    let str = try! NSMutableAttributedString(data: data! as Data, options: [.documentType: NSAttributedString.DocumentType.officeOpenXML, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+    return str
+}
+
+
+
+func listDocsInCurrentDirectory() throws -> [String] {
+    let fileManager = FileManager()
+    let files = try! fileManager.contentsOfDirectory(atPath: ".") 
+    let docs = files.filter({ $0.hasSuffix(".docx") }).sorted(by: <)
+    return docs
+}
+
+func mergeDocs() -> NSMutableAttributedString {
+    let docs = try! listDocsInCurrentDirectory()
+    let first = readMutableDocx(docs[0])
+    let rest = docs[1...]
+    let lineBreaks = NSAttributedString(string: "\n\n")
+    for docToAdd in rest {
+        first.append(lineBreaks)
+        first.append(readDocx(docToAdd))
+    }
+    return first
+}
+
+func toDocx(_ str: NSAttributedString) -> Data {
+    let range = NSMakeRange(0, str.length)
+    let out = try! str.data(from: range, documentAttributes: [.documentType: NSAttributedString.DocumentType.officeOpenXML])
+    return out
+}
+
+let docdata = mergeDocs()
+
+print(docdata.string)
